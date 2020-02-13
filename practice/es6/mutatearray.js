60e5f50438b48ff51a91d112560c3eb1187744e4
irenapopova <irenejpopova6@gmail.com>
2020-02-13 16:01:14 +0100

    ex

---
+++ practice/es6/mutatearray.js
@@ -0,0 +1,80 @@
+//Mutate an Array Declared with const
+/**
+ * However, it is important to understand that objects
+ * (including arrays and functions) assigned to a variable using
+ * const are still mutable. Using the const declaration only
+ * prevents reassignment of the variable identifier.
+ */
+
+// sample
+"use strict";
+const s = [5, 6, 7];
+s = [1, 2, 3]; // throws error, trying to assign a const
+s[2] = 45; // works just as it would with an array declared with var or let
+console.log(s); // returns [5, 6, 45]
+const s = [5, 7, 2];
+/**
+ * you can mutate the object [5, 6, 7] itself and the variable s will still point to the altered
+ * array [5, 6, 45]. Like all arrays, the array elements in s are mutable,
+ * but because const was used, you cannot use the variable identifier s to
+ * point to a different array using the assignment operator.
+ */
+
+// SOLUTION
+function editInPlace() {
+  "use strict";
+  // change code below this line
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+
+  // s = [2, 5, 7]; <- this is invalid
+
+  // change code above this line
+}
+editInPlace();
+
+// Prevent Object Mutation with Object.freeze()
+
+/**
+ * As seen in the previous challenge, 
+ * const declaration alone doesn't really protect your data from mutation. 
+ * To ensure your data doesn't change, JavaScript provides a function 
+ * Object.freeze to prevent data mutation.
+ * Once the object is frozen, you can no longer add, update, 
+ * or delete properties from it. Any attempt at changing the object will be 
+ * rejected without an error.
+
+
+ */
+// SAMPLE
+
+let obj = {
+  name: "Coding",
+  review: "Awesome"
+};
+Object.freeze(obj);
+obj.review = "bad"; // will be ignored. Mutation not allowed
+obj.newProp = "Test"; // will be ignored. Mutation not allowed
+console.log(obj);
+// { name: "Coding", review:"Awesome"}
+
+// SOLUTION
+
+function freezeObj() {
+  "use strict";
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+  // change code below this line
+  Object.freeze(MATH_CONSTANTS);
+
+  // change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch (ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
--- practice/february/feb13.js
+++ practice/february/feb13.js
@@ -107,3 +107,191 @@ function getMountName (Jan =1, Dec = 12)
 
 // Create a function that reverses a string.
 // Throw a custom error if the user inputs another type as an argument.
+
+
+
+// reverse a string
+function ReverseString(str) { 
+  
+  // Check input 
+  if(!str || str.length < 2 ||  
+          typeof str!== 'string') { 
+      return 'Not valid';  
+  } 
+    
+  // Take empty array revArray 
+  const revArray = []; 
+  const length = str.length - 1; 
+    
+  // Looping from the end 
+  for(let i = length; i >= 0; i--) { 
+      revArray.push(str[i]); 
+  } 
+    
+  // Joining the array elements 
+  return revArray.join(''); 
+} 
+
+document.write(ReverseString("coding is fun")) 
+
+// In Javascript strings are immutable (this depends on programming language), 
+//which means string is like an array in the meaning you can access its elements by index. 
+//But it does not allow manipulating individual elements at individual indexes. 
+//If you try this in 'normal' mode, it fill fail silently (it will execute 
+//but string won't be modified and you'll never know why). 
+//If you use strict mode via 'use strict';, it will fail with the following error:
+
+
+// Declare a readonly variable 
+function printManyTimes(str) {
+  "use strict";
+
+  // change code below this line
+
+  const SENTENCE = str + " is cool!";
+  for (let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
+  }
+
+  // change code above this line
+
+}
+printManyTimes("freeCodeCamp");
+
+// Counting Cards
+
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+
+  var msg = '';
+
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 7:
+    case 8:
+    case 9:
+      count = count;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+
+  if (count === 5) msg = "5 Bet";
+  else if (count === 0) msg = "0 Hold";
+  else if (count === -5) msg = "-5 Hold";
+  else if (count === -1) msg = "-1 Hold";
+  else if (count === 1) msg = "1 Bet";
+
+
+  return msg;
+  // Only change code above this line
+}
+
+// Add/remove calls to test your function.
+// Note: Only the last will display
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+// EXERCISE FCC JS basics
+// Replace Loops using Recursion
+/**
+ * a function can be expressed in terms of itself. 
+ * To help understand this, start by thinking about the following task: 
+ * multiply the elements from 0 to n inclusive in an array to create
+ *  the product of those elements. Using a for loop, you could do this:
+ */
+
+ // SAMPLE
+ function multiply(arr, n) {
+  var product = arr[0];
+  for (var i = 1; i <= n; i++) {
+      product *= arr[i];
+  }
+  return product;
+}
+
+// SOLUTION
+
+function sum(arr, n) {
+  // Only change code below this line
+  if(n <= 0) {
+    return arr[0];
+  } else {
+    return sum(arr, n - 1) + arr[n];
+  }
+  // Only change code above this line
+}
+
+// Destructuring Assignment to Extract Values from Objects
+
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// change code below this line
+
+const { today, tomorrow } = HIGH_TEMPERATURES;
+
+// change code above this line
+
+console.log(yesterday) // should be not defined
+console.log(today); // should be 77
+console.log(tomorrow); // should be 80
+
+// CREATE A JAVASCRIPT PROMISE
+
+/**
+ * Promise is a constructor function, so you need to use the new keyword 
+ * to create one. It takes a function, as its argument, 
+ * with two parameters - resolve and reject. These are methods used to 
+ * determine the outcome of the promise. The syntax looks like this:
+ */
+
+const myPromise = new Promise((resolve, reject) => {
+
+});
+
+//Use the Rest Parameter with Function Parameters
+/**
+ * With the rest parameter, you can create functions that take a variable 
+ * number of arguments. These arguments are stored in an array that can be
+ *  accessed later from inside the function.
+ */
+
+function howMany(...args) {
+  return "You have passed " + args.length + " arguments.";
+}
+console.log(howMany(0, 1, 2)); // You have passed 3 arguments.
+console.log(howMany("string", null, [1, 2, 3], { })); // You have passed 4 arguments.
+
+/**
+ * The rest parameter eliminates the need to check the args array and allows us to 
+ * apply map(), filter() and reduce() on the parameters array.
+ */
+
+ // EXERCISE 
+ /**
+  * Modify the function sum using the rest parameter in such a way that the function sum 
+  * is able to take any number of arguments and return their sum.
+  */
+
+  // SOLUTION
+  const sum = (...args) => {
+    return args.reduce((a, b) => a + b, 0);
+  }
+  
+  console.log(sum(1, 2, 3)); // 6
