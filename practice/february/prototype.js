eaa89f3119341904e01106817c57fdd53566f5f7
irenapopova <irenejpopova6@gmail.com>
2020-02-24 08:57:58 +0100

    exercises

---
+++ practice/february/prototype.js
@@ -0,0 +1,29 @@
+// PROTOTYPE OBJECT
+// Prototype Inheritance
+
+// a shared object / sharable objects, they share their
+// properties and their methods, so I can access those members via the instances
+// that I created;
+// how to attach a prototype object to the constructor object
+// this word is based on context, it is all about HOW it is invoked;
+// this key word needs context, if I point to this , what I am pointing to 
+
+// Prototype Constructors
+
+// javascript treats FUNCTIONS as Callable Objects
+
+
+
+
+
+// summery 
+/**
+ * prototypes are just shared objects, they allow me to share properties and methods
+ * across a range of objects,
+ * the prototype Object is attached to the callable object, 
+ * which is the constructor and when I construct an object from 
+ * that constructor , it is automatically linked to 
+ * that callable object prototype, that link is dynamically 
+ * established and then I have one copy and I can use 
+ * this context to go back out to the instance level
+ */,
