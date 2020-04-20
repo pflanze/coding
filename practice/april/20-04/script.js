b203cef34bf70a20cef386324fb418a4a56c7c39
Irena <irenejpopova6@gmail.com>
2020-04-20 10:20:01 +0200

    DOM

---
+++ practice/april/20-04/script.js
@@ -0,0 +1,40 @@
+//   Javascript in the browser
+//  XMl, JSON, jQuery, Ajax, API
+
+//  JS Engines:
+//   V8 (Chrome, Node, Edge)
+//    SpiderMonkey (Firefox)
+//    Chakra (Internet Explorer)
+
+//   DOM aka Document Object Module
+//   DOM tree.
+
+// Adding javascript to HTML:
+// - The `<script>` tag
+// - External JS with `<script src="...">`
+
+// The Window object
+// (objects functions and variables) are member of window object.
+// document object is also member of the window object
+// the window object contain many Properties amd methods
+
+// -Host object-
+// are objects supplied by a certain environment.
+// like in browser environment you will find window
+// in node.js environment you will find NodeList
+
+// -Native object-
+// re standard built-in objects provided by Javascript.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
+
+// window.alert("Hi you are here");
+// let x = window.prompt("enter your name", "Ali");
+// console.log(x);
+
+function change() {
+  window.document.getElementsByTagName("h1")[0].outerHTML =
+    "<h6>Hi, this is me</h6>";
+  let newBox = document.getElementById("main");
+  console.log(newBox);
+  newBox.style.backgroundColor = "red";
+}
