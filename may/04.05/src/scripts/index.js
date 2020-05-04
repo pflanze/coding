12e37f79e9f889b646960f4d19c3205100f9473d
Irena <irenejpopova6@gmail.com>
2020-05-04 09:32:14 +0200

    module scripts

---
+++ may/04.05/src/scripts/index.js
@@ -0,0 +1,13 @@
+// Module Basics:
+//  Using modules in the browser: <script type="module" src="...">
+
+//  Connecting files: The `import` and `export` keywords
+
+//  defer attribute
+// tells the browser that it should go on working with the page, and load the script “in background”, then run the script when it loads.
+// https://javascript.info/script-async-defer#defer
+
+// https://docs.npmjs.com/creating-a-package-json-file
+
+//JavaScript Object Notation (JSON)
+// It's a file format, and data interchange format, that uses human-readable text to store and transmit data objects
