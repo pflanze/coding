dd2b10fb84b041ac8707a09af2f365dd5229b129
Irena <irenejpopova6@gmail.com>
2020-05-25 17:29:56 +0200

    node.js

--- NODE-JS/dig-into-nodeJS/exercises/ex1.js
+++ NODE-JS/dig-into-nodeJS/exercises/ex1.js
@@ -1,2 +1,16 @@
 // Node.js
-// 
+// My  Node.js program
+
+// #!/usr/bin / env node
+
+"use strict";
+
+var path = require("path");
+var fs = require("fs");
+
+var getStdin = require("get-stdin");
+
+var args = require("minimist")(process.argv.slice(2), {
+  boolean: ["help", "in",],
+  string: ["file",],
+});
