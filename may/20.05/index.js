cd9deb9c5946fe5590233e9f7e8bed6f6ad43822
Irena <irenejpopova6@gmail.com>
2020-05-25 08:53:29 +0200

    javascript

--- may/20.05/index.js
+++ may/20.05/index.js
@@ -34,3 +34,43 @@ function callback3(marshmallow) {
 // calling the function
 
 prepareHotChocolate(callback);
+
+// asinc ..await 
+
+
+
+
+
+
+
+//=============================
+// async await teacher solution 
+
+function milkCheck(response) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      if (response == "heated") {
+        resolve("your milk is hot now add chocolate");
+      }
+    }, 3000);
+  });
+}
+function chocolateCheck(response) {
+  return new Promise((resolve) => {
+    resolve(` ${response} now add marshmallow`);
+  });
+}
+function marshmallowCheck(response) {
+  return new Promise((resolve) => {
+    resolve(` ${response} now your drink is ready`);
+  });
+}
+async function prepareHotCocoa() {
+  const step1 = await milkCheck("heated");
+  console.log(step1);
+  const step2 = await chocolateCheck(step1);
+  console.log(step2);
+  const step3 = await marshmallowCheck(step2);
+  console.log(step3);
+}
+prepareHotCocoa();
