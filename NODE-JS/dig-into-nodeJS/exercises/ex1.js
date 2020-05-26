305ca2f838ec3c5f26e86fa690faa73ced2a5784
Irena <irenejpopova6@gmail.com>
2020-05-26 12:20:01 +0200

    node.js added package minimist

--- NODE-JS/dig-into-nodeJS/exercises/ex1.js
+++ NODE-JS/dig-into-nodeJS/exercises/ex1.js
@@ -21,8 +21,8 @@
 
 // to access the stream for standard output use : process.stdout
 
-// accsessing the output streams
-process.stdout.write("Hello World\n");
+// accessing the output streams
+// process.stdout.write("Hello World\n");
 
 // process.stdin.read()
 // #!
@@ -36,10 +36,47 @@ process.stdout.write("Hello World\n");
 
 //console.log(“Hello World”); and console.error(“Oops”);  they direct themselves to two different standard I/O streams, one standard out and one standard error , ?//by default the shell environment is interpreting standard out and standard error outputs both the same and just print them. But they are different by doing some file redirection (file descriptor redirection, some output redirection). 
 
-// always run "use strict" mode
-"use strict";
+// always run my code in "use strict" mode and before "use strict" i can have the line #!/usr/bin / env node
 
-var path = require("path");
+// How to make it executable? // node script / shell script 
+// when writing node scripts to put for myself some kind of help output for my scripts, because I will come back to the script days later and I will forget how I am suppose to puss stuff in or run it. 
+
+//! remember: good idea - always to have some help output. 
+
+// when in exercise folder to run the code in terminal use command : ./ex1.js or if permission is denied use node ex1.js
+//#!/usr/bin / env node
+"use strict";
+// define the function and call it 
+// process.argv is an array of all the arguments that were passed in from the executing shell. So if I were to do console.log(process.argv) see the array and what has been passed to the program. 
+// process.argv (argv - is name from traditional C - programming), it is called argv - for the list of arguments , and arc for the count. 
+// using a package : minimist instead of regular expression and adding an extra parentheses set to execute the function, and I am going to tell it what array I want it to parse which is : process.argv.slice(2), i put it inside the parentheses as an INPUT
+require("minimist")(process.argv.slice(2))
+
+console.log(process.argv.slice(2));
+// with .slice method i slice off the first two arguments
+// write in terminal to call again and pass an extra parameter (node ex1.js --hello=world -c9 -88 -irena -ron (extra parameters ))
+// how command line arguments get specified on the command line?
+// --long parameter name (dash dash long parameter name) or single dash with a short parameter name, single dash with space in between, 
+// there are different  conventions the way parameters get specified and none of that parsing is happening for us by node. I have to type it in terminal.
+// printHelp();
+// regular expression parsing but use a package instead : minimist
+
+
+// *************************
+// function to print basic output
+function printHelp() {
+  console.log("ex1 usage:")
+  console.log(" ex.js --help");
+  console.log("")
+  console.log("--help              print this help");
+  console.log("");
+}
+
+// First thing : how to get some input into my program,
+
+// Ways of getting input from the outer system into my program. one of the most common is to use the command line parameters. ( ./ex1.js --hello=world) in my case in my terminal (node ex1.js --hello=wold) this is the input, the command line parameter that is going to come into my program. I have to be able to access that command line parameter. How can I do that? First I have to access the arguments that were passed to my program by way of another exposure of the POSEX,  which is process.args, args is plural.
+
+/* var path = require("path");
 var fs = require("fs");
 
 var getStdin = require("get-stdin");
@@ -47,4 +84,4 @@ var getStdin = require("get-stdin");
 var args = require("minimist")(process.argv.slice(2), {
   boolean: ["help", "in",],
   string: ["file",],
-}); 
+});  */
