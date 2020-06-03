08b207231db8ce5e26e9ff30d499386dbb9b7c8a
Irena <irenejpopova6@gmail.com>
2020-06-03 09:40:20 +0200

    nodejs practice

--- June/03.06/index.js
+++ June/03.06/index.js
@@ -1,9 +1,23 @@
 const fs = require("fs");
 
+// write listens for an error 
 let writer = fs
   .createWriteStream('newBigText.txt', { flags: "wx+" });
+  // event listener is .on
   .on("error", (err) => {
     console.log("err");
+  })
+  .on('open', function (fileData) {
+    console.log("This is open");
+    console.log(`file data : ${fileData}`);
   });
 
 writer.write("Hi this is new file done by Nodejs");
+
+writer.write(("Hi this is new file done by Nodejs"))
+let newWriter = fs.createWriteStream("newText.txt", { flags: "w" });
+newWriter.on("pipe", () => {
+  console.log("cool something is piped in");
+});
+
+let reader = fs.createReadStream("newBigTest.txt").pipe(newWriter);
