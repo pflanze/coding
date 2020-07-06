d0a2faabd241eb0d6652838bc383cdca18d72ed3
Irena <irenejpopova6@gmail.com>
2020-07-06 15:32:06 +0200

    destructiong

--- July/adopt-me/src/App.js
+++ July/adopt-me/src/App.js
@@ -1,9 +1,38 @@
+// destructuring the core idea of what react is 
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}. [
+    React.createLElement("h2", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+
+  ]);
+};
+
+
 const App = () => {
   return React.createElement(
     "div",
-    {},
-    React.createElement("h1", {}, "Adopt Me!"));
+    { id: "something-important" }, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese"
+    }),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese"
+    }),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese"
+    })
+
+  ]);
 };
+
 ReactDOM.render(
   React.createElement(App),
   document.getElementsById("root"));
